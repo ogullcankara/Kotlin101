4bcc8bd0284b8c1437e9a5413b7a3f9cfb334e6b
ogullcankara <ogullcankara@gmail.com>
2022-05-10 23:40:39 +0300

    Change few files

--- src/Degiskenler.kt
+++ src/Degiskenler.kt
@@ -1,4 +1,16 @@
 fun main() {
     var myInt : Int = 1
     println(myInt)
+
+    var x = 5
+    var y = 6
+    println(x*y)
+
+    val pi = 3.14
+    println(pi * 2)
+
+    var myAge : Double = 24.0
+    println(myAge)
+
+
 }
