8ade1bbba8926712c93e410385ecf3f5ec66cc0d
ogullcankara <ogullcankara@gmail.com>
2022-05-11 00:19:44 +0300

    changes files

--- src/Arrayler.kt
+++ src/Arrayler.kt
@@ -1,4 +1,4 @@
 fun main() {
-    val myExampleArray = arrayOf(1,2,3,4,5)
+    val myExampleArray = arrayOf(1,2,3,4,5,6)
     println("Element 1: ${myExampleArray[1]}")
 }
--- src/Degiskenler.kt
+++ src/Degiskenler.kt
@@ -9,12 +9,14 @@ fun main() {
     val pi = 3.14
     println(pi * 2)
 
-    var myAge : Double = 24.0
+    var myAge : Double = 25.5
     println(myAge)
 
     val myString : String = "FirsString"
     println(myString)
     val myString2 : String = "SeconString"
     println(myString2)
+    val myString3 : String = "SeconString"
+    println(myString3)
 
 }
